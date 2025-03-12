e9cd01a9d0621fde4aefb6d320ed3fd64f3069a6
Shakir <shakirali.sacube@gmail.com>
2025-03-12 22:35:16 +0530

    Fix missing imports on index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,13 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./database/db.js";
 
+import userRoute from "./routes/user.route.js";
+import mediaRoute from "./routes/media.route.js";
+import courseRoute from "./routes/course.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
+// import purchaseRoute from "./routes/purchase.route.js";
+
+
 dotenv.config({});
 
 connectDB();
@@ -23,7 +30,7 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/progress", courseProgressRoute);
-app.use("/api/v1/purchase", purchaseRoute);
+// app.use("/api/v1/purchase", purchaseRoute);
 
 
 
